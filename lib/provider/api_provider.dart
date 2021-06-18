e9a667e5fd542b4d3c381d25e58ebf0b38d113df
@dipak8347 <dipakdarji8347@gmial.com>
2021-06-18 21:53:44 +0530

    Provider

--- lib/provider/api_provider.dart
+++ lib/provider/api_provider.dart
@@ -1,42 +1,64 @@
-// import 'package:final_test/provider/db_provider.dart';
 // import 'dart:convert';
-// import 'package:flutter/services.dart';
+// import 'package:final_test/model/posts.dart';
 // import 'package:http/http.dart' as http;
-// import 'package:http/http.dart';
-//
-// import '../model/posts.dart';
-// import 'db_provider.dart';
-//
-// var url = Uri.parse('https://raw.githubusercontent.com/mwgg/Airports/master/airports.json');
 //
 //
-// class EmployeeApiProvider {
+// class Services {
+//   //
+//   static List<PostModel> albums;
+//   var url = Uri.parse('https://raw.githubusercontent.com/mwgg/Airports/master/airports.json');
 //
-//   Future<List<PostModel>> getAllEmployees() async {
-//     // var url = Uri.parse(
-//     //     'https://raw.githubusercontent.com/mwgg/Airports/master/airports.json');
+//   static Future<PostModel> getData() async {
+//     var url = Uri.parse('https://raw.githubusercontent.com/mwgg/Airports/master/airports.json');
+//     List _dates = [];
+//  //   final List<PostModel> items = [];
 //
-//     Response response = await http.get(url);
-//     Map<String, dynamic> decoded = jsonDecode(response.body).toList();
-//     //debugPrint(response);
-//     return  DBProvider.db.createEmployee(PostModel.fromJson(decoded));
-//     // return employeeFromJson(response.data).map((employee) {
-//     //   print('Inserting $employee');
-//     //   DBProvider.db.createEmployee(employee);
-//     // }).toList();
+//       final response =  await http.get(url);
+//       Map<String, dynamic> decodedDoughnut = jsonDecode(response.body);
+//       if (200 == response.statusCode) {
+//
+//
+//         // print(jsonDecode(decodedDoughnut.toString()));
+//
+//         decodedDoughnut.keys.forEach((String key) {
+//           _dates.add(key);
+//         });
+//         for (int i = 0; i < _dates.length; i++) {
+//           PostModel event = PostModel(
+//
+//             icao: decodedDoughnut[_dates[i]]["icao"].toString(),
+//
+//             name: decodedDoughnut[_dates[i]]["name"].toString(),
+//
+//             city: decodedDoughnut[_dates[i]]["city"].toString(),
+//
+//             state: decodedDoughnut[_dates[i]]["state"].toString(),
+//
+//             country: decodedDoughnut[_dates[i]]["country"].toString(),
+//
+//             elevation: decodedDoughnut[_dates[i]]["elevation"].toString(),
+//
+//             lat: decodedDoughnut[_dates[i]]["lat"].toString(),
+//
+//             lon: decodedDoughnut[_dates[i]]["lon"].toString(),
+//
+//             tz: decodedDoughnut[_dates[i]]["tz"].toString(),
+//
+//           );
+//
+//           PostModel.items.add(event);
+//
+//         }
+//         print(PostModel.items.length);
+//
+//         } else {
+//         PostModel albums = new PostModel();
+//          //= [];
+//         return albums; // we are returning empty album list
+//         // Handle these as you want...
+//       }
+//
 //   }
 //
 //
-//
-//   // Future<List<PostModel>> getAllEmployees() async {
-//   // //  Response response = await Dio().get(url);
-//   //   var response = await http.get(url);
-//   //   Map<String, dynamic> decoded = jsonDecode(response.body).toList();
-//   //
-//   // return  DBProvider.db.createEmployee(PostModel.fromJson(decoded));
-//   //   // return (decoded.data as List).map((employee) {
-//   //   //
-//   //   //   DBProvider.db.createEmployee(PostModel.fromJson(employee));
-//   //   // }).toList();
-//   // }
 // }
--- lib/provider/db_provider.dart
+++ lib/provider/db_provider.dart
@@ -1,68 +1,71 @@
 // import 'dart:async';
-// import 'dart:io';
+// import 'dart:io' as io;
 // import 'package:final_test/model/posts.dart';
 // import 'package:path/path.dart';
-// import 'package:path_provider/path_provider.dart';
 // import 'package:sqflite/sqflite.dart';
+// import 'package:path_provider/path_provider.dart';
 //
+// class DBHelper {
+//   static Database _db;
+//   // Create the Table colums
+//   static const String TABLE = 'airposts';
+//   static const String ID = 'id';
+//   static const String ICAO = 'icao';
+//   static const String NAME = 'name';
+//   static const String CITY = 'city';
+//   static const String STATE = 'state';
+//   static const String COUNTRY = 'country';
+//   static const String ELEVATION = 'elevation';
+//   static const String LAT = 'lat';
+//   static const String LON = 'lon';
+//   static const String TZ = 'tz';
+//   static const String DB_NAME = 'airport.db';
 //
-// class DBProvider {
-//   DBProvider._();
-//
-//   static final DBProvider db = DBProvider._();
-//   Database ?_database;
-//
-//
-//
-//
-//   Future<Database?> get database async {
-//
-//     if (_database != null) return _database;
-//     _database = await initDB();
-//     return _database;
+//   // Initialize the Database
+//   Future<Database> get db async {
+//     if (null != _db) {
+//       return _db;
+//     }
+//     _db = await initDb();
+//     return _db;
 //   }
 //
-//   // Create the database and the Employee table
-//   initDB() async {
-//     Directory documentsDirectory = await getApplicationDocumentsDirectory();
-//     final path = join(documentsDirectory.path, 'product.db');
-//
-//     return await openDatabase(path, version: 1, onOpen: (db) {},
-//         onCreate: (Database db, int version) async {
-//           await db.execute('CREATE TABLE Product('
-//               'id INTEGER PRIMARY KEY,'
-//               'icao TEXT,'
-//               'name TEXT,'
-//               'city TEXT,'
-//               'state TEXT,'
-//               'country TEXT,'
-//               'elevation TEXT,'
-//               'lat TEXT,'
-//               'lon TEXT,'
-//               'tz TEXT'
-//               ')');
-//         });
+//   initDb() async {
+//     // Get the Device's Documents directory to store the Offline Database...
+//     io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+//     String path = join(documentsDirectory.path, DB_NAME);
+//     var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+//     return db;
 //   }
 //
-//   // Insert employee on database
-//   createEmployee(PostModel newEmployee) async {
-//
-//     final db = await database;
-//     final res = await db!.insert('Product', newEmployee.toJson());
-//
-//     return res;
+//   _onCreate(Database db, int version) async {
+//     // Create the DB Table
+//     await db.execute(
+//         "CREATE TABLE $TABLE ($ID INTEGER PRIMARY KEY, $ICAO TEXT, $NAME TEXT, $CITY TEXT, $STATE TEXT,$COUNTRY TEXT,$ELEVATION TEXT,$LAT TEXT,$LON TEXT,$TZ TEXT)");
 //   }
 //
-//   // Delete all employees
 //
 //
-//   Future<List<PostModel>> getAllEmployees() async {
-//     final db = await database;
-//     final res = await db!.rawQuery("SELECT * FROM PRODUCT");
+//   // Method to return all Albums from the DB
+//   Future<PostModel> getAlbums() async {
+//     var dbClient = await db;
+//     // specify the column names you want in the result set
+//     List<Map> maps = await dbClient.query(TABLE, columns: [ID, ICAO, NAME, CITY, STATE, COUNTRY, ELEVATION, LAT, LON, TZ ]);
+//     PostModel allAlbums = PostModel();
+//     List<PostModel> albums = [];
+//     if (maps.length > 0) {
+//       for (int i = 0; i < maps.length; i++) {
+//         albums.add(PostModel.fromJson(maps[i]));
+//       }
+//     }
+//     //
+//     // allAlbums = albums;
+//     return allAlbums;
+//   }
 //
-//     List<PostModel> list =
-//     res.isNotEmpty ? res.map((c) => PostModel.fromJson(c)).toList() : [];
-//
-//     return list;
+//   // Method to Close the Database
+//   Future close() async {
+//     var dbClient = await db;
+//     dbClient.close();
 //   }
 // }
